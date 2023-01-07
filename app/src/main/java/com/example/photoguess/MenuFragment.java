d658ad19a2b352fdb77e5ae8f006828f5d65dae9
May Shech <may745@gmail.com>
2023-01-07 12:25:37 +0200

    hello nimi

--- app/src/main/java/com/example/photoguess/MenuFragment.java
+++ app/src/main/java/com/example/photoguess/MenuFragment.java
@@ -12,11 +12,13 @@ import android.view.ViewGroup;
 
 public class MenuFragment extends Fragment {
 
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        view = inflater.inflate(R.layout.fragment_menu, container, false);
+        return view;
     }
 }
