28bb455e3f729159e46ecfd3f8274c60103fca85
Nimi <Nimrobo@Gmail.com>
2023-01-07 13:02:32 +0200

    Fragments fixed

--- app/src/main/java/com/example/photoguess/SettingsFragment.java
+++ app/src/main/java/com/example/photoguess/SettingsFragment.java
@@ -3,20 +3,41 @@ package com.example.photoguess;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SettingsFragment extends Fragment {
 
     View view;
+    Button HomeBTN;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_settings, container, false);
+        HomeBTN = view.findViewById(R.id.homeButton);
+        HomeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new MenuFragment());
+            }
+        });
         return view;
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFragmentContainerView, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
 }
