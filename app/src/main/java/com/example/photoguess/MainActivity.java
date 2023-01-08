bccf66ccc6a7d23ef9fe87431491473cbc4c3b8b
May Shech <may745@gmail.com>
2023-01-08 15:03:49 +0200

    Added firebase to the project

--- app/src/main/java/com/example/photoguess/MainActivity.java
+++ app/src/main/java/com/example/photoguess/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.inputmethod.InputMethodManager;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
