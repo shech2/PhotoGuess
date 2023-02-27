026836994dd340347ddd85c8259dd424495c0060
Nimi <Nimrobo@Gmail.com>
2023-02-27 17:30:40 +0200

    'Letter Guess' and 'Full Guess' are now fully functioning

--- app/src/main/java/com/example/photoguess/ActivePlayersFragment.java
+++ app/src/main/java/com/example/photoguess/ActivePlayersFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +35,7 @@ public class ActivePlayersFragment extends Fragment {
     String photoCaptionText;
     char[] photoCaptionArray;
     char[] guessingArray;
+    char[] usedLetters;
     String guessingArrayString;
     DatabaseReference roomRef;
     FirebaseDatabase database;
@@ -49,6 +51,7 @@ public class ActivePlayersFragment extends Fragment {
         roomPin = savedInstanceState.getString("roomPin");
         binding = FragmentActivePlayersBinding.inflate(inflater, container, false);
         view = binding.getRoot();
+        binding.displayedImage.setImageResource(R.drawable.questionmark);
         database = FirebaseDatabase.getInstance("https://photoguess-6deb1-default-rtdb.europe-west1.firebasedatabase.app/");
         roomRef = database.getReference("Rooms").child("Room_" + roomPin);
         roomRef.child("Caption").addListenerForSingleValueEvent(new ValueEventListener(){
@@ -68,13 +71,25 @@ public class ActivePlayersFragment extends Fragment {
         });
         storage = FirebaseStorage.getInstance("gs://photoguess-6deb1.appspot.com");
         storageRef = storage.getReference().child("Room_" + roomPin);
+
         storageRef.getBytes(1024 * 1024).addOnSuccessListener(bytes -> {
+            SystemClock.sleep(1000);
             Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
             binding.displayedImage.setImageBitmap(bitmap);
             binding.displayedImage.setRenderEffect(RenderEffect.createBlurEffect(100, 100, Shader.TileMode.MIRROR));
-            binding.displayedImage.setVisibility(View.VISIBLE);
         }).addOnFailureListener(exception -> Toast.makeText(getContext(), "Download test", Toast.LENGTH_SHORT).show());
-
+        binding.letterGuessButton.setOnClickListener(v -> {
+            String letter = binding.guessText.getText().toString();
+            if (letter.length() == 1) {
+                letterChecker(letter.charAt(0));
+            } else {
+                Toast.makeText(getContext(), "Please enter a single letter", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.fullGuessButton.setOnClickListener(v -> {
+            String guess = binding.guessText.getText().toString();
+            fullGuessChecker(guess);
+        });
 
 
         return view;
@@ -88,5 +103,72 @@ public class ActivePlayersFragment extends Fragment {
                 guessingArray[i] = ' ';
             }
         }
+    }
+
+    public void letterChecker(char letter){
+        if (!Character.isLetter(letter)) {
+            Toast.makeText(getContext(), "Please enter a letter", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        letter = Character.toUpperCase(letter);
+        if (usedLetters != null) {
+            for (char usedLetter : usedLetters) {
+                if (usedLetter == letter) {
+                    Toast.makeText(getContext(), "You have already used this letter", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        }
+        boolean letterFound = false;
+        for (int i = 0; i < photoCaptionArray.length; i++) {
+            if (photoCaptionArray[i] == letter) {
+                letterFound = true;
+                guessingArray[i] = letter;
+            }
+        }
+        if (letterFound) {
+            if (usedLetters == null)
+                usedLetters = new char[1];
+            else
+                usedLetters = new char[usedLetters.length + 1];
+            usedLetters[usedLetters.length - 1] = letter;
+            guessingArrayString = new String(guessingArray);
+            binding.hangmanText.setText(guessingArrayString);
+        } else {
+            // TODO: Next player turn, weaken blur
+        }
+    }
+
+    public void fullGuessChecker(String guess) {
+        guess = guess.toUpperCase().trim();
+        if (isAlphabeticalEnglish(guess)){
+            if (guess.equals(photoCaptionText)) {
+                guessingArray = photoCaptionArray;
+                guessingArrayString = new String(guessingArray);
+                binding.hangmanText.setText(guessingArrayString);
+            }
+        }
+    }
+
+    public boolean isAlphabeticalEnglish(String text) {
+        if (text == null || text.isEmpty()) {
+            Toast.makeText(getContext(), "Please enter a caption", Toast.LENGTH_SHORT).show();
+            binding.guessText.setError("Please enter a caption");
+            binding.guessText.requestFocus();
+            return false;
+        }
+        if (text.trim().length() > 30){
+            binding.guessText.setError("Caption is too long");
+            binding.guessText.requestFocus();
+            return false;
+        }
+        for (char c : text.toCharArray()) {
+            if (!Character.isLetter(c) && !Character.isWhitespace(c)) {
+                binding.guessText.setError("Please enter an alphabetical-only caption");
+                binding.guessText.requestFocus();
+                return false;
+            }
+        }
+        return true;
     }
 }
